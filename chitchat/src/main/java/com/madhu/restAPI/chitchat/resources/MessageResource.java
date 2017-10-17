056b59eb652de08418b6dcbae71af7f37f5684d9
madhu <madhusudan923.mr@gmail.com>
2017-10-17 12:35:26 -0400

    Returning XML format.Added model and service classes.

--- chitchat/src/main/java/com/madhu/restAPI/chitchat/resources/MessageResource.java
+++ chitchat/src/main/java/com/madhu/restAPI/chitchat/resources/MessageResource.java
@@ -1,17 +1,23 @@
 package com.madhu.restAPI.chitchat.resources;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.madhu.restAPI.chitchat.model.Message;
+import com.madhu.restAPI.chitchat.service.MessageService;
+
 @Path("/messages")
 public class MessageResource {
+	MessageService messageService= new MessageService();
 	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN )
-	public String getMessages() {
-		return "Hello World";
+	@Produces(MediaType.APPLICATION_XML)
+	public List<Message> getMessages() {
+		return messageService.getAllMessages();
 	}
 
 }
